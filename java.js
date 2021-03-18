0d05a116c5ebec43be849e46cb95a67fec6e4c65
Roko03 <rokoponjarac2@gmail.com>
2021-03-18 23:41:05 +0100

    first commit

---
+++ java.js
@@ -0,0 +1,7 @@
+function sideMenu() {
+    document.getElementById('sidemenu').style.marginLeft = '0';
+}
+
+function closeSideMenu() {
+    document.getElementById('sidemenu').style.marginLeft = '-250px';
+}
